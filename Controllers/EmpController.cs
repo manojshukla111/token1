8d32b3815407c36ffa73a9533452c2ee4088990b
manoj <118164597+manojshukla111@users.noreply.github.com>
2023-08-04 17:15:33 +0530

    Add files via upload

---
+++ Controllers/EmpController.cs
@@ -0,0 +1,29 @@
+﻿using JWT_Token_01.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Token_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+         public string GetData() {
+
+            return "Authenticated with JWT";
+        }
+        [Authorize]
+        [HttpPost]
+
+     //   [Route("PostData")]
+
+        public string addUser(Users user)
+        {
+            return "user added with username" + user.Username;
+        }
+    }
+}
---
+++ Model/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace JWT_Token_01.Model
+{
+    public class Users
+    {
+       
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
